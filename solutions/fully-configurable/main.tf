eb8a00f9fb3e54dc25633018303450463e9faaf7
Arya Girish K <arya.girish.k@ibm.com>
2026-03-10 16:24:18 +0530

    testing

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -474,13 +474,13 @@ locals {
 
 data "external" "install_required_binaries" {
   count   = var.install_required_binaries ? 1 : 0
-  program = ["bash", "-c", "curl -sS https://raw.githubusercontent.com/terraform-ibm-modules/terraform-ibm-icd-elasticsearch/issue-16994/solutions/fully-configurable/scripts/install-binaries.sh | bash -s ${local.binaries_path}"]
+  program = ["bash", "-c", "curl -sS https://raw.githubusercontent.com/terraform-ibm-modules/terraform-ibm-icd-elasticsearch/issue-16994/solutions/fully-configurable/scripts/install-binaries.sh -o /tmp/install-binaries.sh && chmod +x /tmp/install-binaries.sh && /tmp/install-binaries.sh ${local.binaries_path} && echo '{}'"]
 }
 
 data "external" "es_metadata" {
   depends_on = [data.external.install_required_binaries]
   count      = var.enable_kibana_dashboard ? 1 : 0
-  program    = ["bash", "-c", "curl -sS https://raw.githubusercontent.com/terraform-ibm-modules/terraform-ibm-icd-elasticsearch/issue-16994/solutions/fully-configurable/scripts/es_metadata.sh | bash -s ${local.binaries_path}"]
+  program    = ["bash", "-c", "curl -sS https://raw.githubusercontent.com/terraform-ibm-modules/terraform-ibm-icd-elasticsearch/issue-16994/solutions/fully-configurable/scripts/es_metadata.sh -o /tmp/es_metadata.sh && chmod +x /tmp/es_metadata.sh && bash /tmp/es_metadata.sh"]
   query = {
     url         = "https://${local.elasticsearch_hostname}:${local.elasticsearch_port}"
     username    = local.elasticsearch_username
